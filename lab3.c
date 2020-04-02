5cfd0d3baade245d30173de17ddd714a9b1e73d8
Kevin Conyers <kgc0019@tigermail.auburn.edu>
2020-04-02 05:29:29 -0500

    Initial Commit, Working ish

---
+++ lab3.c
@@ -0,0 +1,265 @@
+
+/*****************************************************************************\
+ * Laboratory Exercises COMP 3510                                              *
+ * Author: Saad Biaz                                                           *
+ * Date  : April 1, 2014                                                   *
+ \*****************************************************************************/
+
+/*****************************************************************************\
+ *                             Global system headers                           *
+ \*****************************************************************************/
+
+
+#include "common3.h"
+
+/*****************************************************************************\
+ *                             Global data types                               *
+ \*****************************************************************************/
+
+
+
+/*****************************************************************************\
+ *                             Global definitions                              *
+ \*****************************************************************************/
+#define MAX_QUEUE_SIZE  2
+
+
+
+
+
+
+/*****************************************************************************\
+ *                            Global data structures                           *
+ \*****************************************************************************/
+
+
+
+
+/*****************************************************************************\
+ *                                  Global data                                *
+ \*****************************************************************************/
+double EventsTotalPerDevice[MAX_NUMBER_DEVICES] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+double EventsMissedPerDevice[MAX_NUMBER_DEVICES] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+double EventsServedPerDevice[MAX_NUMBER_DEVICES] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+double MissPercentagePerDevice[MAX_NUMBER_DEVICES] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+double AvgTurnaroundTimePerDevice[MAX_NUMBER_DEVICES] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+double AvgResponseTimePerDevice[MAX_NUMBER_DEVICES] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+
+Event DeviceQueues[MAX_NUMBER_DEVICES][MAX_QUEUE_SIZE];
+int QueueSizes[MAX_NUMBER_DEVICES]  = {0, 0, 0, 0, 0, 0, 0, 0};
+int NextStorage[MAX_NUMBER_DEVICES] = {0, 0, 0, 0, 0, 0, 0, 0};
+int NextEvent[MAX_NUMBER_DEVICES] = {0, 0, 0, 0, 0, 0, 0, 0};
+
+Status DeviceHasEventWaiting = 0;
+
+int HIGHEST_PRIORITY_DEVICE_INDEX = 0;
+int LOWEST_PRIORITY_DEVICE_INDEX = 1; //in the case of 2 devices, number is always NUM_DEVICE-1
+
+
+
+
+/*****************************************************************************\
+ *                               Function prototypes                           *
+ \*****************************************************************************/
+
+void Control(void);
+void InterruptRoutineHandlerDevice(void);
+void BookKeeping();
+void EnqueueEvent(Status Flags);
+void DequeueHighestPriorityEvent();
+
+
+
+
+/*****************************************************************************\
+ * function: main()                                                            *
+ * usage:    Create an artificial environment for embedded systems. The parent *
+ *           process is the "control" process while children process will gene-*
+ *           generate events on devices                                        *
+ ******************************************************************************** Inputs: ANSI flat C command line parameters                                 *
+ * Output: None                                                                *
+ *                                                                             *
+ * INITIALIZE PROGRAM ENVIRONMENT                                              *
+ * START CONTROL ROUTINE                                                       *
+ \*****************************************************************************/
+
+int main (int argc, char **argv) {
+
+	LOWEST_PRIORITY_DEVICE_INDEX = (*argv[0]) - 1;
+	if (Initialization(argc,argv)){
+		Control();
+	} 
+} /* end of main function */
+
+
+
+/***********************************************************************\
+ * Input : none                                                          *
+ * Output: None                                                          *
+ * Function: Monitor Devices and process events (written by students)    *
+ \***********************************************************************/
+void Control(void){
+
+	while (1) 
+	{
+		DequeueHighestPriorityEvent();
+	}
+
+}
+
+int deviceLookup(Status Flags)
+{
+	unsigned int device_index = 0;
+	while( Flags >>=1)
+	{
+		device_index++;
+	}
+	return device_index;
+}
+
+void EnqueueEvents(Status deviceQueue) 
+{
+	int device = 0;
+	while(deviceQueue >> device > 0)
+	{
+		if(deviceQueue & (1 << device) == 1)
+		{
+			EnqueueEvent(device);
+		}
+
+	}
+
+}
+
+void EnqueueEvent(Status Flags)
+{
+	unsigned int device_index = 0;
+	while (device_index < MAX_NUMBER_DEVICES) {
+		if(Flags & (1 << device_index))
+		{
+			break;
+		}
+		device_index++;
+	}
+	EventsTotalPerDevice[device_index] = EventsTotalPerDevice[device_index] + 1;
+	Event eventToBeQueued = BufferLastEvent[device_index];
+	if (QueueSizes[device_index] >= MAX_QUEUE_SIZE)
+	{
+		AvgResponseTimePerDevice[device_index] += Now() - eventToBeQueued.When;
+		printf("Dropped Even for device: %d", device_index);
+		//event dropped
+	}
+	else 
+	{
+		//printf("Enqueueing Event for device: %d, at position %d\n", device_index, device_index);
+		AvgResponseTimePerDevice[device_index] += Now() - eventToBeQueued.When;
+		DisplayEvent('+', &eventToBeQueued);
+		DeviceQueues[device_index][NextStorage[device_index]] = eventToBeQueued;
+		NextStorage[device_index] = (NextStorage[device_index] + 1) & (MAX_QUEUE_SIZE - 1); 
+
+		QueueSizes[device_index]++;
+		printf("Enqueued Event for device: %d, at position %d\n", device_index, NextStorage[device_index]); 
+	}
+
+}
+
+void DequeueHighestPriorityEvent()
+{
+	int device_index = 0;
+	while(QueueSizes[device_index] == 0 && device_index < MAX_NUMBER_DEVICES) {
+		device_index++;
+	}
+	if(device_index >= MAX_NUMBER_DEVICES) {
+
+	} else 
+	{
+
+		printf("Deqeueing Event for device: %d Postion: %d\n", device_index, NextEvent[device_index]);
+		Server(&DeviceQueues[device_index][NextEvent[device_index]]);
+		AvgTurnaroundTimePerDevice[device_index] += Now() - DeviceQueues[device_index][NextEvent[device_index]].When;
+		EventsServedPerDevice[device_index]++;
+		QueueSizes[device_index]--;
+		if (QueueSizes[device_index] == 0) 
+		{
+			DeviceHasEventWaiting = DeviceHasEventWaiting & ~(1<<device_index); 
+		}
+		NextEvent[device_index] = (NextEvent[device_index] + 1) & (MAX_QUEUE_SIZE - 1);
+	}
+}
+
+
+/***********************************************************************\
+ * Input : None                                                          *
+ * Output: None                                                          *
+ * Function: This routine is run whenever an event occurs on a device    *
+ *           The id of the device is encoded in the variable flag        *
+ \***********************************************************************/
+void InterruptRoutineHandlerDevice(void){
+	printf("An event occured at %f  Flags = %d \n", Now(), Flags);
+	// Put Here the most urgent steps that cannot wait
+	EnqueueEvent(Flags);
+	DeviceHasEventWaiting |= Flags;
+	Flags = Flags && 0;
+}
+
+
+/***********************************************************************\
+ * Input : None                                                          *
+ * Output: None                                                          *
+ * Function: This must print out the number of Events buffered not yet   *
+ *           not yet processed (Server() function not yet called)        *
+ \***********************************************************************/
+void BookKeeping(void){
+	// For EACH device, print out the following metrics :
+	// 1) the percentage of missed events, 2) the average response time, and 
+	// 3) the average turnaround time.
+	// Print the overall averages of the three metrics 1-3 above
+
+	//Check for starved events
+
+	int i =0;	
+	for (i = 0; i < MAX_NUMBER_DEVICES; i++)
+	{
+		while(QueueSizes[i] > 0) 
+		{
+			AvgTurnaroundTimePerDevice[i] += Now() - DeviceQueues[i][NextEvent[i]].When;
+			QueueSizes[i]--;
+			NextEvent[i] = (NextEvent[i] + 1) & (MAX_QUEUE_SIZE - 1);
+
+		}
+	}
+
+
+	int low = 0;
+	for (i = 0; i < MAX_NUMBER_DEVICES; i++){
+		if(EventsTotalPerDevice[i] > 0) 
+		{
+			low = i;
+
+			EventsMissedPerDevice[i] = EventsTotalPerDevice[i] - EventsServedPerDevice[i]; 
+			MissPercentagePerDevice[i] = EventsMissedPerDevice[i]/EventsTotalPerDevice[i];
+			AvgResponseTimePerDevice[i] = AvgResponseTimePerDevice[i] / EventsTotalPerDevice[i]; 
+			AvgTurnaroundTimePerDevice[i] = AvgTurnaroundTimePerDevice[i] / EventsTotalPerDevice[i]; 
+		}
+	}
+
+	printf("Metrics for Device 0 (highest priority)\n\t+ Avg Response Time: %f\n\t+ Avg Turnaround Time: %f\n\t+ Miss Percentage: %f\n", AvgResponseTimePerDevice[0], AvgTurnaroundTimePerDevice[0], MissPercentagePerDevice[0]);
+	printf("\t+ Total Events: %d\n\t+ Events Missed: %d\n", EventsTotalPerDevice[0], EventsMissedPerDevice[0]);
+	printf("\n\t***\t\n");
+	printf("Metrics for Device %d (lowest priority)\n\t+ Avg Response Time: %f\n\t+ Avg Turnaround Time: %f\n\t+ Miss Percentage: %f\n", low, AvgResponseTimePerDevice[low], AvgTurnaroundTimePerDevice[low], MissPercentagePerDevice[low]);
+	printf("\t+ Total Events: %d\n\t+ Events Missed: %d\n", EventsTotalPerDevice[low], EventsMissedPerDevice[low]);
+
+	printf("\n\t***\t\n");
+
+	for ( i = 1; i < low; i++){
+
+		printf("Metrics for Device %d (middle priority)\nAvg Response Time: %f\nAvg Turnaround Time: %f\nMiss Percentage: %f\n", i, AvgResponseTimePerDevice[i], AvgTurnaroundTimePerDevice[i], MissPercentagePerDevice[i]);
+		printf("\n\t***\t\n");
+	}
+}
+
+
+
+
+
+
